559d1285ee8ed9ce9c9118328db978b6c2e94f18
RichardMin <richardmin7104@gmail.com>
2023-12-18 08:56:40 +0630

    added UI

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -2,7 +2,7 @@
 
 import { Fragment, useCallback, useContext } from "react"
 import { Fragment_054be72e0ea98018059eac5951b3d7a7 } from "/utils/stateful_components"
-import { Box, Button, Container, Spacer, Text } from "@chakra-ui/react"
+import { Box, Container, Image, Spacer, Text } from "@chakra-ui/react"
 import "focus-visible/dist/focus-visible"
 import { EventLoopContext, StateContexts } from "/utils/context"
 import { Event } from "/utils/state"
@@ -10,61 +10,77 @@ import NextHead from "next/head"
 
 
 
-export function Text_27fe4e2e07b6c5b0105696d034f9eb81 () {
+export function Text_79ec486b04aae4fe923c7dd5bb643e7e () {
   const state__state = useContext(StateContexts.state__state)
 
 
   return (
-    <Text sx={{"fontSize": "30px"}}>
-  {state__state.player_choice}
+    <Text sx={{"fontSize": "20px", "fontWeight": "bold", "marginLeft": "270px", "marginTop": "20px"}}>
+  {`You choose >> ${state__state.player_choice}`}
 </Text>
   )
 }
 
-export function Text_9eceaeb5f337f008d6b4e39a0599b629 () {
-  const state__state = useContext(StateContexts.state__state)
-
-
-  return (
-    <Text sx={{"fontSize": "30px"}}>
-  {state__state.com_choice}
-</Text>
-  )
-}
-
-export function Button_b17c392012581ec8f73f0329a6baf518 () {
-  const [addEvents, connectError] = useContext(EventLoopContext);
-
-  const on_click_5ae3dcfd007d2c7375e42abc259bd7f5 = useCallback((_e) => addEvents([Event("state.state.paper_btn", {})], (_e), {}), [addEvents, Event])
-
-  return (
-    <Button onClick={on_click_5ae3dcfd007d2c7375e42abc259bd7f5}>
-  {`Paper`}
-</Button>
-  )
-}
-
-export function Button_a6871ec304ced608a5ca88273d823905 () {
+export function Image_a528a4d6c5dee89e051fc8afffabccf9 () {
   const [addEvents, connectError] = useContext(EventLoopContext);
 
   const on_click_2a0258d878502770ba96be75b0030ffc = useCallback((_e) => addEvents([Event("state.state.scissor_btn", {})], (_e), {}), [addEvents, Event])
 
   return (
-    <Button onClick={on_click_2a0258d878502770ba96be75b0030ffc}>
-  {`Scissor`}
-</Button>
+    <Image onClick={on_click_2a0258d878502770ba96be75b0030ffc} src={`./scissor.png`} sx={{"width": "120px", "height": "120px", "cursor": "pointer", "bg": "#a37cf0", "padding": "20px", "borderRadius": "30px", "boxShadow": "0px 15px 25px rgba(0,0,0,0.15)"}}/>
   )
 }
 
-export function Button_d823cb8df26f4126f3c28893f59e875c () {
+export function Image_30ea6de0236ed2045288a4acff6e11da () {
+  const [addEvents, connectError] = useContext(EventLoopContext);
+
+  const on_click_5ae3dcfd007d2c7375e42abc259bd7f5 = useCallback((_e) => addEvents([Event("state.state.paper_btn", {})], (_e), {}), [addEvents, Event])
+
+  return (
+    <Image onClick={on_click_5ae3dcfd007d2c7375e42abc259bd7f5} src={`./paper.png`} sx={{"width": "120px", "height": "120px", "cursor": "pointer", "marginRight": "30px", "bg": "#a37cf0", "padding": "20px", "borderRadius": "30px", "boxShadow": "0px 15px 25px rgba(0,0,0,0.15)"}}/>
+  )
+}
+
+export function Text_a2750b16feb2890347f8b28b9bf7aa7a () {
+  const state__state = useContext(StateContexts.state__state)
+
+
+  return (
+    <Text sx={{"fontSize": "20px", "fontWeight": "bold", "marginLeft": "230px"}}>
+  {`Computer choosed >> ${state__state.com_choice}`}
+</Text>
+  )
+}
+
+export function Text_2d818e13124867c7fa8713dade289954 () {
+  const state__state = useContext(StateContexts.state__state)
+
+
+  return (
+    <Text sx={{"fontSize": "20px", "fontWeight": "bold", "marginLeft": "305px", "width": "120px", "height": "40px", "bg": "#d8b6e0", "borderRadius": "3px", "marginTop": "30px", "textAlign": "center", "paddingTop": "5px", "boxShadow": "0px 15px 25px rgba(0,0,0,0.15)"}}>
+  {`${state__state.result}`}
+</Text>
+  )
+}
+
+export function Image_23db8d548c592ba386d2356d7747c212 () {
   const [addEvents, connectError] = useContext(EventLoopContext);
 
   const on_click_162b59bdbf8002eab52b192cc6d5b31a = useCallback((_e) => addEvents([Event("state.state.rock_btn", {})], (_e), {}), [addEvents, Event])
 
   return (
-    <Button onClick={on_click_162b59bdbf8002eab52b192cc6d5b31a}>
-  {`Rock`}
-</Button>
+    <Image onClick={on_click_162b59bdbf8002eab52b192cc6d5b31a} src={`./rock.png`} sx={{"width": "120px", "height": "120px", "cursor": "pointer", "marginRight": "30px", "bg": "#a37cf0", "padding": "20px", "borderRadius": "30px", "boxShadow": "0px 15px 25px rgba(0,0,0,0.15)"}}/>
+  )
+}
+
+export function Text_df1fa83439c358e2ebb94126b502e666 () {
+  const state__state = useContext(StateContexts.state__state)
+
+
+  return (
+    <Text sx={{"fontSize": "30px"}}>
+  {`${state__state.win}`}
+</Text>
   )
 }
 
@@ -73,17 +89,21 @@ export default function Component() {
   return (
     <Fragment>
   <Fragment_054be72e0ea98018059eac5951b3d7a7/>
-  <Container>
-  <Text sx={{"fontSize": "32px", "color": "black", "width": "2000px"}}>
+  <Container sx={{"bg": "#e0e0e0", "height": "100vh", "maxWidth": "auto", "position": "relative", "placeItems": "center"}}>
+  <Text sx={{"fontSize": "45px", "color": "black", "width": "2000px", "fontWeight": "bold", "marginLeft": "280px"}}>
   {`Rock,Paper,Scissor game with Nextpy`}
 </Text>
-  <Text_27fe4e2e07b6c5b0105696d034f9eb81/>
-  <Text_9eceaeb5f337f008d6b4e39a0599b629/>
+  <Box sx={{"width": "700px", "height": "500px", "bg": "white", "marginLeft": "315px", "marginTop": "40px", "boxShadow": "0px 15px 25px rgba(0,0,0,0.15)", "borderRadius": "10px"}}>
   <Spacer/>
-  <Box>
-  <Button_d823cb8df26f4126f3c28893f59e875c/>
-  <Button_b17c392012581ec8f73f0329a6baf518/>
-  <Button_a6871ec304ced608a5ca88273d823905/>
+  <Box sx={{"display": "flex", "marginLeft": "150px", "paddingTop": "130px"}}>
+  <Text_df1fa83439c358e2ebb94126b502e666/>
+  <Image_23db8d548c592ba386d2356d7747c212/>
+  <Image_30ea6de0236ed2045288a4acff6e11da/>
+  <Image_a528a4d6c5dee89e051fc8afffabccf9/>
+</Box>
+  <Text_79ec486b04aae4fe923c7dd5bb643e7e/>
+  <Text_a2750b16feb2890347f8b28b9bf7aa7a/>
+  <Text_2d818e13124867c7fa8713dade289954/>
 </Box>
 </Container>
   <NextHead>
--- nextpy_RPS/nextpy_RPS.py
+++ nextpy_RPS/nextpy_RPS.py
@@ -9,17 +9,39 @@ class State(xt.State):
     choices: list = ["Rock","Paper","Scissor"]
     com_choice:str
     player_choice:str
-    def computer_choice(self):
-        self.com_choice = random.choice(self.choices),
+    result:str
+    score:str
+    win = 0
+    lose = 0
+    
+    
+    def game_condition(self):
         
-        xt.text(
-            self.com_choice,
-            font_size= "30px",
-        ),
+        if self.player_choice == self.com_choice:
+            self.result = "Tie"
+        elif self.player_choice == "Rock" and self.com_choice == "Paper":
+            self.result = "You Lose"
+        elif self.player_choice == "Paper" and self.com_choice == "Scissor":
+            self.result = "You Lose"
+        elif self.player_choice == "Scissor" and self.com_choice == "Rock":
+            self.result = "You Lose"
+        else:
+            self.result = "You Win"
+            
+        
+            
+            
+            
+    
     def rock_btn(self):
-        self.player_choice = "rock",
-        self.com_choice = random.choice(self.choices),
-        
+        self.player_choice = "Rock"
+        self.com_choice = random.choice(self.choices)
+        if self.result == "You Win":
+            self.win += 1
+            
+        elif self.result == "You Lose":
+            self.lose += 1
+        self.game_condition
         xt.text(
             self.com_choice,
             font_size= "30px",
@@ -27,28 +49,53 @@ class State(xt.State):
         xt.text(
             self.player_choice,
             font_size= "30px",
-        )
+        ),
+        xt.text(
+            self.result,
+            font_size= "30px",
+        ),
+        xt.text(
+            self.win,
+            font_size= "30px",
+        ),
+        
         
     def paper_btn(self):
-        self.player_choice = "paper",
+        self.player_choice = "Paper",
         self.com_choice = random.choice(self.choices),
-        
-        xt.text(
-            self.com_choice,
-            font_size= "30px",
-        ),
+        self.game_condition()
+        if self.result == "You Win":
+            self.win += 1
+            
+        elif self.result == "You Lose":
+            self.lose += 1
         xt.text(
             self.player_choice,
             font_size= "30px",
         ),
         xt.text(
             self.com_choice,
+            font_size= "30px",
+        ),
+        xt.text(
+            self.result,
+            font_size= "30px",
+        ),
+        xt.text(
+            self.win,
             font_size= "30px",
         ),
 
     def scissor_btn(self):
-        self.player_choice = "scissor",
+        self.player_choice = "Scissor",
         self.com_choice = random.choice(self.choices),
+        self.game_condition()
+        if self.result == "You Win":
+            self.win += 1
+            
+        elif self.result == "You Lose":
+            self.lose += 1
+        
         xt.text(
             self.com_choice,
             font_size= "30px",
@@ -58,9 +105,14 @@ class State(xt.State):
             font_size= "30px",
         ),
         xt.text(
-            self.com_choice,
+            self.result,
             font_size= "30px",
         ),
+        xt.text(
+            self.win,
+            font_size= "30px",
+        ),
+
         
         
 
@@ -70,37 +122,104 @@ def index() -> xt.Component:
     return xt.container(
         xt.text(
            "Rock,Paper,Scissor game with Nextpy",
-           font_size = "32px",
+           font_size = "45px",
            color = "black",
            width = "2000px",
+           font_weight = "bold",
+           margin_left = "280px"
        ),
-        xt.text(
-           State.player_choice,
-           font_size = "30px",
-       ),
-        xt.text(
-           State.com_choice,
-           font_size = "30px",
-       ),
+        
+        xt.box(
         
        xt.spacer(),
        
         xt.box(
-          xt.button(
-           "Rock",
-           on_click = State.rock_btn,
-        ),  
-          xt.button(
-           "Paper",
-           on_click = State.paper_btn,
-        ),  
-          xt.button(
-           "Scissor",
-           on_click = State.scissor_btn,
-        ),  
+          xt.text(
+          f"{State.win}",
+          font_size = "30px",
         ),
-
-
+          xt.image(
+           src="./rock.png",
+           width="120px",
+           height="120px",
+           on_click = State.rock_btn,
+           cursor="pointer",
+           margin_right="30px",
+           bg="#a37cf0",
+           padding="20px",
+           border_radius="30px",
+           box_shadow ="0px 15px 25px rgba(0,0,0,0.15)",
+           
+        ),  
+          xt.image(
+           src="./paper.png",
+           width="120px",
+           height="120px",
+           on_click = State.paper_btn,
+           cursor="pointer",
+           margin_right="30px",
+           bg="#a37cf0",
+           padding="20px",
+           border_radius="30px",
+           box_shadow ="0px 15px 25px rgba(0,0,0,0.15)",
+        ),  
+          xt.image(
+           src="./scissor.png",
+           width="120px",
+           height="120px",
+           on_click = State.scissor_btn,
+           cursor="pointer",
+           bg="#a37cf0",
+           padding="20px",
+           border_radius="30px",
+           box_shadow ="0px 15px 25px rgba(0,0,0,0.15)",
+        ),  
+        display="flex",
+        margin_left = "150px",
+        padding_top="130px"
+        ),
+        xt.text(
+          f"You choose >> {State.player_choice}",
+          font_size="20px", 
+          font_weight ="bold",
+          margin_left="270px",
+          margin_top="20px"
+        ),
+        xt.text(
+          f"Computer choosed >> {State.com_choice}",
+          font_size="20px", 
+          font_weight ="bold",
+          margin_left="230px",
+        ),
+        xt.text(
+          f"{State.result}",
+          font_size="20px", 
+          font_weight ="bold",
+          margin_left="305px",
+          width="120px",
+          height ="40px",
+          bg="#d8b6e0",
+          border_radius="3px",
+          margin_top="30px",
+          text_align="center",
+          padding_top="5px",
+          box_shadow ="0px 15px 25px rgba(0,0,0,0.15)",
+        ),
+            width="700px",
+            height="500px",
+            bg="white",
+            margin_left = "315px",
+            margin_top="40px",
+            box_shadow ="0px 15px 25px rgba(0,0,0,0.15)",
+            border_radius = "10px"
+        ),
+       
+        bg="#e0e0e0",
+        height="100vh",
+        max_width = "auto",
+        position = "relative",
+        place_items = "center",
+        
     )
 
 
